bd05d6ed7e7638b280d89e04076ead349c0476fa
Poliakov Kirill <kirde001@gmail.com>
2024-04-08 19:37:48 +0300

    first

---
+++ Step 2/PrintingBrrr.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){                             
+     fmt.Println("row1")
+     fmt.Println("row2")   
+     fmt.Println("row3")
+     fmt.Print("row4")
+}
